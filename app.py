77437d19d34d6dd906aafa38e25a0c4468a1b212
adxam1257-cpu <adxam1257@gmail.com>
2026-06-17 01:21:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+from PIL import Image
 
+# Load model
+model = tf.keras.models.load_model("model.h5")
+
+class_names = [
+    "T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
+    "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"
+]
+
+st.title("Fashion Product Classification (CNN + AI)")
+
+st.write("Upload an image and the model will predict the fashion category.")
+
+uploaded_file = st.file_uploader("Choose an image", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).convert("L")
+    image = image.resize((28, 28))
+
+    img_array = np.array(image) / 255.0
+    img_array = img_array.reshape(1, 28, 28, 1)
+
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    prediction = model.predict(img_array)
+    predicted_class = np.argmax(prediction)
+
+    st.write("### Prediction:", class_names[predicted_class])
+    st.write("Confidence:", float(np.max(prediction)))
